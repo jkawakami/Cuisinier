ae861ffbad0c93e427cf597aeae2869c0a47cd31
Alan Kha <akhahaha@gmail.com>
2015-11-13 11:41:52 -0800

    Update skeleton code
    - Add cuisineCount tracking
    - Move JSON parser functions outside of Cuisinier module
    - Implement selfTest() function in main module

--- Cuisinier.py
+++ Cuisinier.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import json
 import logging
 from collections import namedtuple
 
@@ -16,22 +15,30 @@ Uses ingredients to categorize the cuisine of a recipe.
 class Cuisinier:
     def __init__(self):
         self.recipes = {}  # <id, Recipe>
+        self.cuisineCount = {}  # <cuisine, frequency>
         self.cuisineMatrix = {}  # <cuisine, <ingredient, frequency>>
+        self.ingredientCount = {}  # <ingredient, frequency>
         self.ingredientMatrix = {}  # <ingredient, <cuisine, frequency>>
-        self.ingredientHistogram = {}  # <ingredient, global frequency>
 
     """
     Adds a ClassifiedRecipe to the knowledge base.
     @param  recipe  ClassifiedRecipe Recipe to be added
     @return         True if successful, false otherwise
     """
-    def add(self, recipe: ClassifiedRecipe):
+    def addRecipe(self, recipe: ClassifiedRecipe):
         if not isinstance(recipe, ClassifiedRecipe):
-            raise TypeError("Cuisinier.add() takes a ClassifiedRecipe")
+            raise TypeError("Cuisinier.addRecipe() takes a ClassifiedRecipe")
 
         # Add new recipes to knowledge base
         if recipe.id not in self.recipes:
             self.recipes[recipe.id] = recipe
+
+            # Add to ingredient counter
+            if recipe.cuisine not in self.cuisineCount:
+                self.cuisineCount[recipe.cuisine] = 0
+            self.cuisineCount[recipe.cuisine] += 1
+
+            # Initialize cuisine matrix entry
             if recipe.cuisine not in self.cuisineMatrix:
                 self.cuisineMatrix[recipe.cuisine] = {}
 
@@ -42,17 +49,17 @@ class Cuisinier:
                     self.cuisineMatrix[recipe.cuisine][ingredient] = 0
                 self.cuisineMatrix[recipe.cuisine][ingredient] += 1
 
+                # Add to ingredient counter
+                if ingredient not in self.ingredientCount:
+                    self.ingredientCount[ingredient] = 0
+                self.ingredientCount[ingredient] += 1
+
                 # Add to ingredient matrix
                 if ingredient not in self.ingredientMatrix:
                     self.ingredientMatrix[ingredient] = {}
                 if recipe.cuisine not in self.ingredientMatrix[ingredient]:
                     self.ingredientMatrix[recipe.cuisine] = 0
                 self.ingredientMatrix[recipe.cuisine] += 1
-
-                # Add to global ingredient histogram
-                if ingredient not in self.ingredientHistogram:
-                    self.ingredientHistogram[ingredient] = 0
-                self.ingredientHistogram[ingredient] += 1
 
             # Return true if successful
             logging.info("Add recipe " + str(recipe.id) + ":\tSUCCESS")
@@ -62,44 +69,34 @@ class Cuisinier:
         return False
 
     """
-    Add ClassfiedRecipes from a training JSON file.
-    @param  file    Training JSON filepath
+    Add a list of ClassfiedRecipes.
+    @param  recipes List of ClassifedRecipes
     """
-    def addFile(self, file):
-        # Read and parse JSON data
-        recipes = json.loads(open(file).read())
-
-        # Iterate through recipes
+    def addRecipes(self, recipes):
         success = 0
         for recipe in recipes:
-            if (self.add(ClassifiedRecipe(recipe["id"], recipe["cuisine"],
-                                          recipe["ingredients"]))):
+            if (self.addRecipe(recipe)):
                 success += 1
 
-        logging.info(file + ": " + str(success) + "/" +
-                     str(len(recipes)) + " successful")
+        logging.info(str(success) + "/" + str(len(recipes)) + " recipes added")
 
     """
     Classifies a given Recipe.
     @param  recipe  Recipe to be classified
     @return         ClassfiedRecipe
     """
-    def classify(self, recipe: Recipe):
+    def classifyRecipe(self, recipe: Recipe):
         if not isinstance(recipe, Recipe):
-            raise TypeError("Cuisinier.classify() takes a Recipe")
+            raise TypeError("Cuisinier.classifyRecipe() takes a Recipe")
 
         # TODO Perform classification
         return ClassifiedRecipe(recipe.id, "unknown", recipe.ingredients)
 
     """
-    Classifies the recipes from a JSON file.
-    @param  file    Unclassified recipe JSON filepath
-    @return         Array of ClassfiedRecipes
+    Classifies a list of Recipes.
+    @param  recipes List of Recipes
+    @return         List of ClassfiedRecipes
     """
-    def classifyFile(self, file):
-        # Read and parse JSON data
-        recipes = json.loads(open(file).read())
-
+    def classifyRecipes(self, recipes):
         # Iterate through recipes
-        return [self.classify(Recipe(recipe["id"], recipe["ingredients"]))
-                for recipe in recipes]
+        return [self.classifyRecipe(recipe) for recipe in recipes]
--- main.py
+++ main.py
@@ -1,13 +1,54 @@
 # -*- coding: utf-8 -*-
 
+import json
 import logging
-import Cuisinier
+
+from Cuisinier import Recipe
+from Cuisinier import ClassifiedRecipe
+from Cuisinier import Cuisinier
+
+LOGGING_LEVEL = logging.INFO
+TRAINING_FILE = "resources/train.json"
+TEST_FILE = "resources/test.json"
 
 # Configure logging
-LOGGING_LEVEL = logging.INFO
 logging.basicConfig(filename="log.txt", filemode="w", level=LOGGING_LEVEL)
 
-cuisinier = Cuisinier.Cuisinier()
-cuisinier.addFile("resources/train.json")
-for recipe in cuisinier.classifyFile("resources/test.json"):
-    print(str(recipe.id) + ":\t" + recipe.cuisine)
+
+def getClassifiedRecipes(file):
+    f = open(file)
+    recipes = json.loads(f.read())
+    f.close()
+    return [ClassifiedRecipe(recipe["id"], recipe["cuisine"],
+                             recipe["ingredients"])
+            for recipe in recipes]
+
+
+def getRecipes(file):
+    f = open(file)
+    recipes = json.loads(f.read())
+    f.close()
+    return [Recipe(recipe["id"], recipe["ingredients"]) for recipe in recipes]
+
+
+def selfTest():
+    # Read and parse JSON data
+    recipes = getClassifiedRecipes(TRAINING_FILE)
+    cuisinier = Cuisinier()
+    cuisinier.addRecipes(recipes)
+
+    success = 0
+    for recipe in recipes:
+        result = cuisinier.classifyRecipe(Recipe(recipe.id,
+                                                 recipe.ingredients))
+        if result.cuisine == recipe.cuisine:
+            success += 1
+        print(str(result.id) + ":\t" + result.cuisine + " / " + recipe.cuisine)
+
+    print("Self-test: " + str(success) + "/" + str(len(recipes)))
+
+
+def main():
+    selfTest()
+
+main()
